772eaf0228cc90f03c930490024689e5f2c355c6
alexandr.poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2017-05-27 17:01:33 +0300

    separated todo component logic into new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { TodoModule } from './components/todo/todo.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { TodoListComponent } from './components/todo/todo-list/todo-list.component';
-import { TodoFormComponent } from './components/todo/todo-form/todo-form.component';
-import { TodoItemComponent } from './components/todo/todo-item/todo-item.component';
-import { TodoService } from 'app/components/todo/services/todo.service';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './shared/data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
-    TodoListComponent,
-    TodoFormComponent,
-    TodoItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 0 })
+    TodoModule
   ],
-  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/components/todo/todo.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { TodoComponent } from './todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoService } from './services/todo.service';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from '../../shared/data.service';
+
+@NgModule({
+  declarations: [
+    TodoComponent,
+    TodoListComponent,
+    TodoFormComponent,
+    TodoItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 0 })
+  ],
+  providers: [TodoService],
+  exports: [TodoComponent]
+})
+export class TodoModule {
+}
